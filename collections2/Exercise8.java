1fee5f9f1c4f026537338bf3960b55b724580e08
Ganesh Nitta <nittaganeshkumar@gmail.com>
2023-03-10 14:22:51 +0000

    collections2

---
+++ collections2/Exercise8.java
@@ -0,0 +1,17 @@
+//  Write a Java program to iterate through all elements in a tree set.
+
+import java.util.TreeSet;
+public class Exercise8 {
+  public static void main(String[] args) {
+  TreeSet<String> tree_set = new TreeSet<String>();
+  tree_set.add("Red");
+  tree_set.add("Green");
+  tree_set.add("Orange");
+  tree_set.add("White");
+  tree_set.add("Black");
+  // Print the tree list
+  for (String element : tree_set) {
+    System.out.println(element);
+    }
+ }
+}
