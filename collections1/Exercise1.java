b415faf7bda22bfbd4e51e5368e88cf91b0f6674
Ganesh Nitta <nittaganeshkumar@gmail.com>
2023-03-10 03:54:43 +0000

    Exercise1

---
+++ collections1/Exercise1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Exercise1 {
+  public static void main(String[] args) {
+    List<String> List_String = new ArrayList<String>();
+    List_String.add("Black");
+    List_String.add("White");
+    List_String.add("Green");
+    List_String.add("Blue");
+    List_String.add("Red");
+    List_String.add("Orange");
+    System.out.println(List_String);
+ }
+}
